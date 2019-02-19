b917d6d8aec93825646c7a102494b1d70815265b
Raj Malakpet <rmalakpet@juniper.net>
2019-02-19 11:30:04 -0800

    made it a web service

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 8080;
+require('dotenv').config()
 var bodyParser = require('body-parser')
+var TokenCache = require('google-oauth-jwt').TokenCache,
+tokens = new TokenCache();
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('/webhook', (req, res) => {
-    console.log('/webhook got hit with req: ')
-    if (!req.body) return res.sendStatus(400)
-    res.setHeader('Content-Type', 'application/json')
-    console.log('received the data: ', req.body);
-    let response = 'raj malakpet worked';
-    let responseObj = {
-        "fulfillmentText": response,
-        "fulfillmentMessages": [{"text":{"text":[" Worked "]}}],
-        "source": ""
-    }
-    console.log(responseObj)
-    return res.json(responseObj)
-})
+app.get("/getToken", (req, res, next) => {
+    console.log('<=== /getToken was hit ===>');
+    new Promise((resolve) => {
+            tokens.get({
+                // use the email address of the service account, as seen in the API console
+                email: process.env.email,
+                // use the PEM file we generated from the downloaded key
+                key: (process.env.NODE_ENV == "production") ? JSON.parse(process.env.key) : process.env.key,
+                // specify the scopes you wish to access
+                scopes: ['https://www.googleapis.com/auth/cloud-platform']
+            },
+            (err, token) => {
+                console.log('<=== check error and token: '+err+', '+token);
+                if(typeof(token) !== "undefined" && token !== null){
+                    res.json({"token": token});
+                }else if(err){
+                    res.json({"token": "error"});
+                }else{
+                    res.json({"token": "error"});
+                }
+                resolve(token);
+            }
+        );
+    });
+});
 
 app.listen(port, () => {
     console.log('app is running on port: ', port);
